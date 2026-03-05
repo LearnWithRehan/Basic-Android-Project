8768d7125caaffb13841b6893a5e3937fab74a97
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-03-05 12:26:07 +0530

    Report Screen working

--- KuhuInvoicePro/app/src/main/java/com/example/kuhuinvoicepro/DateReportAdapter.java
+++ KuhuInvoicePro/app/src/main/java/com/example/kuhuinvoicepro/DateReportAdapter.java
@@ -32,9 +32,12 @@ public class DateReportAdapter extends RecyclerView.Adapter<DateReportAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         InvoiceWithItems data = list.get(position);
-
-        holder.txtCustomer.setText("Customer : " + data.invoice.customerName);
         holder.txtDate.setText("Date : " + data.invoice.date);
+        holder.txtCustomer.setText("Customer : " + data.invoice.customerName);
+        holder.txtAddress.setText("Address: " + data.invoice.customerAddress);
+        holder.txtMobile.setText("Mobile: " + data.invoice.customerMobile);
+        holder.txtNetAmt.setText("Total Amount: " + data.invoice.totalAmount);
+        holder.txtDiscount.setText("Discount: " + data.invoice.discount);
         holder.txtAmount.setText("Net Amount : ₹ " + data.invoice.netAmount);
 
         StringBuilder items = new StringBuilder();
@@ -43,7 +46,9 @@ public class DateReportAdapter extends RecyclerView.Adapter<DateReportAdapter.Vi
 
             items.append(item.productName)
                     .append(" | Qty: ")
-                    .append(item.qty)
+                    .append(item.qty +"kg")
+                    .append(" | Rate: ")
+                    .append(item.rate)
                     .append(" | ₹")
                     .append(item.amount)
                     .append("\n");
@@ -60,14 +65,18 @@ public class DateReportAdapter extends RecyclerView.Adapter<DateReportAdapter.Vi
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtCustomer, txtDate, txtAmount, txtItems;
+        TextView txtCustomer,txtAddress,txtMobile,txtNetAmt, txtDate, txtAmount,txtDiscount, txtItems;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             txtCustomer = itemView.findViewById(R.id.txtCustomer);
+            txtAddress = itemView.findViewById(R.id.txtAddress);
+            txtMobile = itemView.findViewById(R.id.txtMobile);
+            txtNetAmt = itemView.findViewById(R.id.txtNetAmt);
             txtDate = itemView.findViewById(R.id.txtDate);
             txtAmount = itemView.findViewById(R.id.txtAmount);
+            txtDiscount = itemView.findViewById(R.id.txtDisconut);
             txtItems = itemView.findViewById(R.id.txtItems);
         }
     }
